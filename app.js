707629e21faf3decad8568469191d49ac498f6d2
Hiroyuki Kusu <kusu0806@gmail.com>
2014-11-25 20:50:02 +0900

    tweet logic

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var app = express();
 
@@ -11,3 +12,33 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
+*/
+
+var Twit = require('twit');
+var CronJob = require("cron").CronJob;
+//require('date-utils');
+var moment = require('moment');
+
+var T = new Twit({
+  consumer_key: 'aSLuztuX7B4950nZGOEeZIkDO'
+  , consumer_secret: 'T9PlOk7tRc9xxqFHhGmQE4x3nysrkzQN0NO0OzNBo6hJeISPx1'
+  , access_token: '2418610800-DiJzhMOPGOWoDvAHxO7fUzqsUid9FteEamFKtxO'
+  , access_token_secret: 'ZESfr7znD0w6Vy35R3bLEpgzZtjfvBfj8nPylIT7pWs5w'
+});
+
+var C = new CronJob({
+  cronTime: '0 * * * * *',
+  onTick: function () {
+    tweet();
+  },
+  start: true
+});
+
+function tweet(){
+  var message = moment().format("現在、MM月DD日 HH時mm分です。");
+  console.log(message);
+
+  T.post('statuses/update', { status: message }, function(err, data, response) {
+    console.log('Tweet！');
+  });
+}
